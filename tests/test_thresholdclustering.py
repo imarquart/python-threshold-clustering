91d86eaf3901fc48aeb723598166827731131bbc
Giulio Rossetti <giulio.rossetti@gmail.com>
2021-06-25 09:40:15 +0200

    :new: conda

--- tests/test_thresholdclustering.py
+++ tests/test_thresholdclustering.py
@@ -1,6 +1,6 @@
 import community as community_louvain
-import matplotlib.cm as cm
-import matplotlib.pyplot as plt
+#import matplotlib.cm as cm
+#import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 
@@ -18,18 +18,18 @@ G=nx.from_numpy_matrix(cos_Adj)
 
 pos = nx.spring_layout(G)
 weights = np.array([G[u][v]['weight'] for u,v in G.edges()])*5
-nx.draw_networkx_nodes(G, pos, node_size=40)
-nx.draw_networkx_edges(G, pos, alpha=0.2, width=weights)
-plt.show()
+#nx.draw_networkx_nodes(G, pos, node_size=40)
+#nx.draw_networkx_edges(G, pos, alpha=0.2, width=weights)
+#plt.show()
 
 
 partition=community_louvain.best_partition(G.to_undirected())
 
-cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
-nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
-                       cmap=cmap, node_color=list(partition.values()))
-nx.draw_networkx_edges(G, pos, alpha=0.2,width=weights)
-plt.show()
+#cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
+#nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
+                     #  cmap=cmap, node_color=list(partition.values()))
+#nx.draw_networkx_edges(G, pos, alpha=0.2,width=weights)
+#plt.show()
 
 
 
@@ -42,9 +42,9 @@ partition, alpha = best_partition(G, cluster_function=cluster_function)
 # draw the graph
 #pos = nx.spring_layout(G)
 # color the nodes according to their partition
-cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
-nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
-                       cmap=cmap, node_color=list(partition.values()))
-nx.draw_networkx_edges(G, pos, alpha=0.2,width=weights)
-plt.show()
+#cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
+#nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
+                      # cmap=cmap, node_color=list(partition.values()))
+#nx.draw_networkx_edges(G, pos, alpha=0.2,width=weights)
+#plt.show()
 
